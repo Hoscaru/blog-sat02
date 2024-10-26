97caf3747862650bbc94cca28727b6ea1c8975dd
Hoscar <oscarodriguez.05.18@gmail.com>
2024-10-26 09:30:25 -0400

    saturday 26

--- registro/views.py
+++ registro/views.py
@@ -10,6 +10,5 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
 
-class LogOutView(TemplateView):
-    template_name = "registration/logout.html"
-
+def FirstPage(request):
+    return render(request, "first_page.html")
